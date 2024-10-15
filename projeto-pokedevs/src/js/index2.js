608499f3782977a4fbcae154d4ffa1b8d3ec7af5
DiegoNotreve <notreveogeide@gmail.com>
2024-10-15 01:00:04 -0300

    testando

---
+++ projeto-pokedevs/src/js/index2.js
@@ -0,0 +1,15 @@
+// Selecionando as imagens com seus respectivos IDs
+const umbrella = document.getElementById('umbrella');
+
+
+// Adicionando o evento de clique no botão
+document.getElementById('transitionButton').addEventListener('click', function() {
+  // Adiciona a animação a ambas as imagens
+  umbrella.classList.add('grow');
+  
+  
+  // Espera a animação de crescimento antes de redirecionar
+  setTimeout(function() {
+    window.location.href = "/projeto-pokedevs/src/page2.html"; // Redireciona para a nova página
+  }, 1000);
+});
